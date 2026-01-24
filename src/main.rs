d0010898e549d03335fba984e60b8bbacf754890
= <alexanderima@outlook.es>
2026-01-24 16:15:25 -0700

    structure

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 use yew::prelude::*;
 
-
 #[component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
 
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class={classes!("app-layout")}> 
         </div>
     }
 }
